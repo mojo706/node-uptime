0b8409f2252bc133ef58d58a0a53949318d2f1bb
mojo706 <xriseugene@gmail.com>
2020-08-19 23:45:26 +0300

    add skeleton server, doesn't do much yet

---
+++ index.js
@@ -0,0 +1,42 @@
+/*
+ *Primary file for the API
+ *
+ */
+
+//Dependencies
+const http = require("http");
+const url = require("url");
+
+// The server should respond to all requests with a string
+const server = http.createServer((req, res) => {
+  //Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  // Get the HTTP method
+  const method = req.method.toUpperCase();
+
+  // Send the response
+  res.end("Hello World\n");
+
+  // Log the request path
+  console.log(
+    "Request is received on path: " +
+      trimmedPath +
+      " with method: " +
+      method +
+      " with this query string parameters: ",
+    queryStringObject
+  );
+});
+
+// Start the server and have it listen on port 3000
+server.listen(3000, () => {
+  console.log("The server is listening on port 3K now");
+});
